64181576b4cbe3c6b8af12921143154a85dbc0d5
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-06-08 21:13:54 +0000

    Add an instance creator based on kwargs dictionary to __init__

--- models/base_model.py
+++ models/base_model.py
@@ -4,30 +4,48 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """BaseModel class for the HBNB"""
 
-    def __init__(self):
-    """Instantiation of a new BaseModel"""
+    def __init__(self, *args, **kwargs):
+        """Instantiation of a new BaseModel"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    continue
+
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    continue
+
+                if key != "__class__":
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
-    """Overwrites the string commands associated with object"""
+        """Overwrites the string commands associated with object"""
 
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-    """Saves updated datetime after any updates to object"""
+        """Saves updated datetime after any updates to object"""
 
         self.updated_at = datetime.now()
 
     def to_dict(self):
-    """Returns a dictionary of the object"""
+        """Returns a dictionary of the object"""
 
-        self.__dict__['__class__'] = self.__class__.__name__
-        self.__dict__['created_at'] = self.created_at.isoformat()
-        self.__dict__['updated_at'] = self.updated_at.isoformat()
-        return self.__dict__
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
