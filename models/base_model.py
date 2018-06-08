391a0e9f24cdb4fa6b84677702723e961606cc0c
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-07 18:42:43 -0700

    added task 4

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,20 @@ class BaseModel:
     """
     Instantiation of class BaseModel
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        initializing var
+        initializing variables
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
