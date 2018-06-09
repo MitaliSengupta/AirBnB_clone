0a78391a407b93978e9670fa03d393c5623faf97
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-06-09 00:32:00 +0000

    Update models package to connect file storage scheme

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Instantiation file"""
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -11,15 +12,16 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instantiation of a new BaseModel"""
 
-
         if len(kwargs) > 0:
             for key, value in kwargs.items():
                 if key == "created_at":
-                    self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    self.created_at = datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
                     continue
 
                 if key == "updated_at":
-                    self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    self.updated_at = datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
                     continue
 
                 if key != "__class__":
@@ -29,6 +31,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """Overwrites the string commands associated with object"""
@@ -40,6 +43,8 @@ class BaseModel:
         """Saves updated datetime after any updates to object"""
 
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary of the object"""
