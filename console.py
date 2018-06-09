b8e5bf018eb205f29c5af1843a7d71637eb1ab73
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-09 11:57:00 -0700

    added console task 7

--- console.py
+++ console.py
@@ -1,39 +1,82 @@
 #!/usr/bin/python3
-"""Entry point for the command interpreter of the HBNB application"""
-
-
+"""
+This module contains the command
+line interpreter
+"""
 import cmd
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
+    """
+    class the custom command interpreter inherits
+    from cmd
+    """
+    prompt = '(hbnb) '
+    classes = ('BaseModel')
 
-    def __init__(self):
-        cmd.Cmd.__init__(self)
-        self.prompt = '(hbnb) '
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel, saves it
+        (to the JSON file) and prints the id. Ex: $ create BaseModel
+        """
+        if not args:
+            print("** class name missing **")
+            return
+        elif args not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            new = eval(args)()
+            new.save()
+            print(new.id)
 
-    def do_EOF(self, line):
-        """Typing the command 'EOF' will cleanly exit the console session"""
+    def do_show(self, args):
+        """
+        Prints the string representation of an instance based
+        on the class name and id. Ex: $ show BaseModel 1234-1234-1234
+        """
+        if len(args.split()) == 0:
+            print("** class name missing **")
+            return
+        elif len(args.split()) < 2:
+            print("** instance id missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            try:
+                with open(self.storage.__FileStorage__file_path,
+                          encoding="UTF-8") as myfile:
+                    dump = json.load(myfile)
+            except FileNotFoundError:
+                dump = None
+                print("** no instance found **")
+                return
 
-        return True
+    def do_quit(self, args):
+        """
+        Quit command to exit out of the interpreter
+        """
+        quit()
 
-    def do_quit(self, line):
-        """Typing 'quit' will cleanly exit the console session"""
+    def do_EOF(self, args):
+        """
+        EOF command to exit out of intepreter
+        """
+        quit()
 
-        return True
-
-    def do_create(self, line):
-
-    def do_show(self, line):
-
-    def do_destroy(self, line):
-
-    def do_all(self, line):
-
-    def do_update(self, line):
+    def do_help(self, args):
+        """
+        Help command to get more information on
+        input commands if no parameter given display
+        all commands
+        """
+        cmd.Cmd.do_help(self, args)
 
     def emptyline(self):
-        """Empty lines will go to the next input loop"""
-
+        """
+        display prompt
+        """
         pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
