5fd316b5930f94f222176f178f4b203a580aa8dc
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-06-09 21:42:06 +0000

    Add console definitions

--- console.py
+++ console.py
@@ -3,6 +3,10 @@
 
 
 import cmd
+import models
+from models.base_model import BaseModel
+
+class_list = {"BaseModel": BaseModel}
 
 class HBNBCommand(cmd.Cmd):
 
@@ -21,14 +25,39 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, line):
+        arg = line.split()
+        names = dir()
+        if (len(arg) == 0):
+            print("** class name is missing **")
+
+        else:
+            if arg[0] in class_list:
+                new_obj = class_list[arg[0]]()
+                models.storage.save()
+            else:
+                print("** class doesn't exist **")
 
     def do_show(self, line):
+        arg = line.split()
+        try:
+            dict_of_objs = models.storage.all()
+            for key in dict_of_objs.keys():
+                if arg[1] == dict_of_objs[key].id:
+                    print(dict_of_objs[key])
+                    break
+
+
+        except IndexError:
+            print("** instance id missing **")
 
     def do_destroy(self, line):
+        pass
 
     def do_all(self, line):
+        pass
 
     def do_update(self, line):
+        pass
 
     def emptyline(self):
         """Empty lines will go to the next input loop"""
