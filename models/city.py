c1f5e6167d8f6375beeccda9266c19e410b27c48
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-06-09 21:56:55 +0000

    Add various classes that all inherit from BaseModel

---
+++ models/city.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""City clas that inherits from BaseModel"""
+
+class City(BaseModel):
+    """City class that contains a state_id and name"""
+
+    state_id = ""
+    name = ""
