1e78fd2276b9e6131ac5d15f7ff04d5027f0f4e7
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-06-09 21:58:35 +0000

    Fix typo in docstring

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""City clas that inherits from BaseModel"""
+"""City class that inherits from BaseModel"""
 
 class City(BaseModel):
     """City class that contains a state_id and name"""
