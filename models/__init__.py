87964908ee1f5953836141f962da58f903dd98c9
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-09 12:37:25 -0700

    updated init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Instantiation file that starts the file storage system"""
+"""
+Instantiation file that starts the file storage system
+"""
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 storage = FileStorage()
 storage.reload()
