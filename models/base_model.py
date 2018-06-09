a6586853110f48b9fda2d2ec508b240074a018c6
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-06-09 22:01:05 +0000

    add updated files to branch

--- models/base_model.py
+++ models/base_model.py
@@ -1,69 +1,55 @@
 #!/usr/bin/python3
-"""Base Model class for object instances"""
-
 import uuid
 from datetime import datetime
 import models
+"""
+Base class for all models will contain id, created_at
+and updated at attributes. Save() and to_json() methods
+"""
 
 
 class BaseModel:
-    """BaseModel class for the HBNB"""
-
+    """
+    Instantiation of class BaseModel
+    """
     def __init__(self, *args, **kwargs):
-        """Instantiation of a new BaseModel -
-           If given a dictionary, it instantiates an object
-           with the given keys and values. Otherwise, it creates
-           a new object and stores it in the file storage mechanism
         """
-
-        if len(kwargs) > 0:
+        initializing variables
+        """
+        if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at":
-                    self.created_at = datetime.strptime(
-                        value, '%Y-%m-%dT%H:%M:%S.%f')
-                    continue
-
-                if key == "updated_at":
-                    self.updated_at = datetime.strptime(
-                        value, '%Y-%m-%dT%H:%M:%S.%f')
-                    continue
-
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """Overwrites the string commands associated with object
-           so that calling the print() function will display the
-           object in a specific format
         """
-
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+        Method returns string representation
+        """
+        return ("[{}] ({}) {}".format(str(type(self).__name__),
+                                      self.id, str(self.__dict__)))
 
     def save(self):
-        """Saves updated datetime after any updates to object
-           and overwrites this object in storage followed by saving
-           in a JSON file
         """
-
+        Method to update attrb updated_at
+        """
         self.updated_at = datetime.now()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary of the object
-           It sets the class name attribute and turns
-           the datetimes into a readable string format
         """
+        Method to return a dict containing all key/value of __dict__
+        instance
+        """
+        dic = dict(**self.__dict__)
+        dic['__class__'] = str(type(self).__name__)
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
 
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        return (dic)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,60 +1,58 @@
 #!/usr/bin/python3
-"""FileStorage class that converts to and from JSON to keep objects
-   persistent
 """
-
-
+This module contains code related to file storage
+for the airbnb clone project. A json data format
+for serialization and deserialization of data.
+"""
 import json
-import os
-from models.base_model import BaseModel
+import models
 
 
 class FileStorage:
-    """FileStorage system class that writes all objects to a file so they are
-       persistent
     """
-
+    This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    """
     __file_path = "file.json"
-    objects = {}
+    __objects = {}
 
     def all(self):
-        """Reloads the JSON file, converts the strings into objects by
-           reinitializing instances, and returns the dictionary of
-           objects
         """
-
-        self.reload()
-        return self.__class__.objects
+        Public instance method to return
+        the dictionary __objects
+        """
+        return (self.__objects)
 
     def new(self, obj):
-        """Creates a new dictionary entry where the key is in a specified
-           format and the object address is stored as the value
         """
-
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__class__.objects[key] = obj
+        Public instance method that
+        sets in __objects with key id
+        """
+        if obj:
+            self.__objects["{}.{}".format(str(type(obj).__name__),
+                                          obj.id)] = obj
 
     def save(self):
-        """Converts the objects to JSON readable dictionaries and stores them
-           in a separate json dictionary then converts them to a JSON string
-           and stores in a file
         """
-
-        json_dict = {}
-        for key in self.__class__.objects.keys():
-            json_dict[key] = self.__class__.objects[key].to_dict()
-        json_string = json.dumps(json_dict)
-        with open(self.__file_path, mode='w', encoding='utf-8') as f:
-            f.write(json_string)
+        Public instance that serializes __objects
+        to the JSON file
+        """
+        dic = {}
+        for id, object in self.__objects.items():
+            dic[id] = object.to_dict()
+        with open(self.__file_path, mode="w", encoding="UTF-8") as myfile:
+            json.dump(dic, myfile)
 
     def reload(self):
-        """Reinitializes the __objects variable by converting the JSON string
-           object into a Python object and then re-instantiating with a kwargs
-           dictionary
         """
-
-        if os.path.isfile(self.__class__.__file_path):
-            with open(self.__file_path, mode='r', encoding='utf-8') as f:
-                json_obj = json.load(f)
-            for key in json_obj.keys():
-                self.__class__.objects[key] = BaseModel(**json_obj[key])
+        Public instance method to deserialize the JSON file
+        to __objects only if file exists
+        """
+        try:
+            with open(self.__file_path, mode="r", encoding="UTF-8") as myfile:
+                object = json.load(myfile)
+            for id, dic in object.items():
+                value = models.BaseModel(**dic)
+                self.__objects[id] = value
+        except FileNotFoundError:
+            pass
