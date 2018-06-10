80ae4dfa4155be98f74fb587700e6631235ade2c
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-10 12:48:58 -0700

    updated console

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ line interpreter
 """
 import cmd
 from models.base_model import BaseModel
+import models
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -13,6 +14,7 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb) '
     classes = ('BaseModel')
+    filepath = storage._FileStorage.__file_path
 
     def do_create(self, args):
         """
@@ -31,17 +33,69 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, args):
         """
-        Prints the string representation of an instance based
-        on the class name and id. Ex: $ show BaseModel 1234-1234-1234
+        prints the string representation of an instance
+
+        usage: show <class_name> <id>
         """
-        if len(args.split()) == 0:
+        tokens = args.split()
+        if len(tokens) == 0:
+            print('** class name missing **')
+        elif tokens[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(tokens) < 2:
+            print('** instance id missing **')
+        else:
+            try:
+                with open(self.filepath, encoding='UTF-8') as myfile:
+                    dump = json.load(myfile)
+            except FileNotFoundError:
+                dump = None
+                print('** no instance found **')
+                return
+            if dump:
+                cls = tokens[0]
+                uid = tokens[1]
+                keys = "{}.{}".format(cls, uid)
+                for key, val in dump.items():
+                    del val['__class__']
+                    if keys in dump.keys():
+                        obj_dict = dump[key]
+                        obj = eval(cls)(**obj_dict)
+                        print(obj)
+                        return
+            print('** no instance found **')
+
+    def do_destroy(self, args):
+        """
+        Destroy command to delete an instance based on class name for id
+        """
+        arg_list = args.split()
+        if len(arg_list) == 0:
             print("** class name missing **")
-            return
-        elif len(args.split()) < 2:
+        elif len(arg_list) < 2:
             print("** instance id missing **")
         elif args[0] not in self.classes:
             print("** class doesn't exist **")
         else:
+            dict_of_objs = models.storage.all()
+            for key in dict_of_objs.keys():
+                if arg_list[1] == dict_of_objs[key].id:
+                    del dict_of_objs[key]
+                    models.storage.save()
+                    return
+            print("** no instance found **")
+
+    def do_all(self, args):
+        """
+        All command to display all objects that currently exist
+        """
+        arg_list = args.split()
+        if len(args) > 0:
+            if args[0] not in self.classes:
+                print("** class doesn't exist **")
+        dict_of_objs = models.storage.all()
+        for key in dict_of_objs.keys():
+            print(dict_of_objs[key])
             try:
                 with open(self.storage.__FileStorage__file_path,
                           encoding="UTF-8") as myfile:
@@ -50,6 +104,15 @@ class HBNBCommand(cmd.Cmd):
                 dump = None
                 print("** no instance found **")
                 return
+
+    def do_update(self, args):
+        """
+        Update command to add or update an attribute
+        """
+        arg_list = args.split()
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+
 
     def do_quit(self, args):
         """
