efdcf7714c4e15aa0b7f3fa140d5bdf02e70eb65
MitaliSengupta <mitali.s.auger@gmail.com>
2018-06-10 19:18:50 -0700

    added place, city, review, state etc

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    class has public attributes
+    """
+    name = ""
