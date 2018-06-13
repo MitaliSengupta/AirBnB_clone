474b0043515dcc4062dc90d03af5d22f66e86d0d
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-06-13 01:21:14 +0000

    Add unittest files for mandatory tasks

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,92 @@
+#!/usr/bin/python3
+"""
+Unittest for BaseModel class
+"""
+import unittest
+import os
+import pep8
+from models.base_model import BaseModel
+from datetime import datetime
 
+
+class TestBaseModel(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.basemodel1 = BaseModel()
+        cls.basemodel1.name = "Derek"
+        cls.basemodel1.age = 24
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.basemodel1
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_style_check(self):
+        """
+        PEP8 style testing
+        """
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/base_model.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_check_for_func(self):
+        """
+        Test function documentation
+        """
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_attr(self):
+        """
+        Test function presence
+        """
+        self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "__str__"))
+        self.assertTrue(hasattr(BaseModel, "save"))
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
+
+    def test_init(self):
+        """
+        Test __init__ function
+        """
+        self.assertTrue(isinstance(self.basemodel1, BaseModel))
+        dictionary = self.basemodel1.to_dict()
+        self.basemodel2 = BaseModel(**dictionary)
+        self.assertTrue(isinstance(self.basemodel2, BaseModel))
+        basemodel2_dict = self.basemodel2.__dict__
+        self.assertIsInstance(basemodel2_dict['created_at'], datetime)
+        self.assertIsInstance(basemodel2_dict['updated_at'], datetime)
+
+    def test_save(self):
+        """
+        Test save function
+        """
+        basemodel1_dict = self.basemodel1.__dict__
+        self.basemodel1.save()
+        object = self.basemodel1
+        self.assertNotEqual(object.created_at, object.updated_at)
+
+    def test_to_dict(self):
+        """
+        Test to_dict function
+        """
+        self.assertEqual(self.basemodel1.__class__.__name__, 'BaseModel')
+        # test before to_dict() call
+        self.assertIsInstance(self.basemodel1.__dict__['created_at'], datetime)
+        self.assertIsInstance(self.basemodel1.__dict__['updated_at'], datetime)
+        # test after to_dict() call
+        basemodel1_dict = self.basemodel1.to_dict()
+        self.assertEqual(basemodel1_dict['__class__'], 'BaseModel')
+        self.assertIsInstance(basemodel1_dict['created_at'], str)
+        self.assertIsInstance(basemodel1_dict['updated_at'], str)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,1 +1,96 @@
+#!/usr/bin/python3
+"""
+Unittests module for State subclass
+"""
+import unittest
+import os
+import pep8
+from models.state import State
+from models.base_model import BaseModel
+from datetime import datetime
+import json
 
+
+class TestState(unittest.TestCase):
+    """
+    Unittests for the State subclass of BaseModel class
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.state = State()
+        cls.state.name = "Illinois"
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.state
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_style_check(self):
+        """
+        PEP8 style tests
+        """
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/state.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_is_subclass(self):
+        """
+        Test for inheritance from BaseModel class
+        """
+        self.assertTrue(issubclass(self.state.__class__, BaseModel), True)
+
+    def test_check_for_func(self):
+        """
+        Tests for documentation of the class
+        """
+        self.assertIsNotNone(State.__doc__)
+
+    def test_has_attr(self):
+        """
+        Tests that proper attributes are present
+        """
+        self.assertTrue('id' in self.state.__dict__)
+        self.assertTrue('created_at' in self.state.__dict__)
+        self.assertTrue('updated_at' in self.state.__dict__)
+        self.assertTrue('name' in self.state.__dict__)
+
+    def test_attr_type(self):
+        """
+        Tests the types of each attribute present
+        """
+        self.assertEqual(type(self.state.name), str)
+        self.assertEqual(type(self.state.created_at), datetime)
+        self.assertEqual(type(self.state.updated_at), datetime)
+
+    def test_save(self):
+        """
+        Test save function of the superclass
+        """
+        self.assertTrue('save' in dir(self.state))
+        self.state.save()
+        self.assertNotEqual(self.state.created_at, self.state.updated_at)
+
+    def test_to_dict_before(self):
+        """
+        Test to_dict function of the superclass
+        """
+        self.assertEqual('to_dict' in dir(self.state), True)
+        self.assertIsInstance(self.state.__dict__['created_at'], datetime)
+        self.assertIsInstance(self.state.__dict__['updated_at'], datetime)
+
+    def test_to_dict_after(self):
+        """
+        Test after to_dict call
+        """
+        self.state_dict = self.state.to_dict()
+        self.assertTrue(json.dumps(self.state_dict))
+        self.assertIsInstance(self.state_dict['created_at'], str)
+        self.assertIsInstance(self.state_dict['updated_at'], str)
+        self.assertTrue(self.state_dict['__class__'] == 'State')
+
+
+if __name__ == "__main__":
+    unittest.main()
