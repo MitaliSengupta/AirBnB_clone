ba55bd44603e278231c3f50e430a1a2fb904d997
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-06-13 01:28:25 +0000

    Fix PEP8 issues on two files

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-  """
-  Amenity class that inherits from BaseModel
-  """
-  name = ""
+    """
+    Amenity class that inherits from BaseModel
+    """
+    name = ""
